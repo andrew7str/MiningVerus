f9848d16e8f81df1bbb2ce9e4747dc4627233e26
Mr.exe <49715949+andrew7str@users.noreply.github.com>
2025-03-30 19:09:08 +0700

    Update start_miningverus.bat

--- start_miningverus.bat
+++ start_miningverus.bat
@@ -1,4 +1,5 @@
 @echo off
+title Mining Verus Solo By Mr.exe
 cd /d %~dp0
 SRBMiner-MULTI.exe --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RJx5vfWFAfGBSkbH6HjGgmxLA2cKcqQ9Bc --password x
 pause
